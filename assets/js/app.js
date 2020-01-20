f198105a966e062e8000d843a3a65f9f08fc19e9
drthisguy <page.c.tyler@gmail.com>
2020-01-20 13:13:03 -0500

    moved HW-7 repo to my main github io page. Set everything up in a MDB environment and animated the hamburger button that opens the collapsible navbar.

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  //to annimate the nabar-dropdown burger.
+  $('.burger-button').on('click', () => {
+    $('.animated-burger').toggleClass('open');
+  });
+
+
+});
