71f057aac34a88b6b02bb92fc0009bd4fe66a8be
drthisguy <page.c.tyler@gmail.com>
2020-03-20 19:13:39 -0400

    resized logo images for faster loading.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
 
-    //to annimate the nabar-dropdown burger.
+    //to animate the navbar-dropdown burger.
     $('.burger-button').on('click', () => {
       $('.animated-burger').toggleClass('open');
     });
+
+    // $('#submit').on('click', function() {
+    // const name = $('#name').val().trim(),
+    //  email = $('#email').val().trim(),
+    //  message = $('#message').val().trim();
   
+    //  if(!name  ||  !email  ||  !message)  {
+    //      alert( "Please confirm you input")
+    //  }  else {
+    //      $.ajax({
+    //          url:  "https://formspree.io/page.c.tyler@gmail.com",
+    //          method: "POST",
+    //          data: $(this).serialize(),
+    //          dataType: 'json'
+    //      })
+        
+    //     alert('Message Sent');
+    //  }
+    // })
 });
