a73787d5a81fe3f81be286a65d6c8862223b54a6
drthisguy <page.c.tyler@gmail.com>
2020-03-20 03:23:26 -0400

    created new js files for the contact and porfolio page.

---
+++ assets/js/portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    //to annimate the nabar-dropdown burger.
+    $('.burger-button').on('click', () => {
+      $('.animated-burger').toggleClass('open');
+    });
+  
+});
